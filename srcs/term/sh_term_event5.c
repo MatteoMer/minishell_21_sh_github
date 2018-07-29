d6da50dc029ac5f3ee918c06d2046f34ac9a1e1c
Matteo <matteomerv@gmail.com>
2018-07-29 18:24:58 +0200

    fix copy-paste crash

--- srcs/term/sh_term_event5.c
+++ srcs/term/sh_term_event5.c
@@ -6,7 +6,7 @@
 /*   By: mmervoye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/29 15:31:00 by mmervoye          #+#    #+#             */
-/*   Updated: 2018/07/29 15:37:44 by mmervoye         ###   ########.fr       */
+/*   Updated: 2018/07/29 18:20:55 by mmervoye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int					term_evt_paste(t_term *term)
 {
 	char		line[LINE_MAX];
 
+	if ((term->line_edit.len) + ft_strlen(term->c_buffer) >= LINE_MAX)
+		return (-1);
 	ft_strcpy(line, term->line_edit.text + term->line_edit.cur);
-	ft_bzero(term->line_edit.text + term->line_edit.cur, ft_strlen(term->line_edit.text + term->line_edit.cur));
+	ft_bzero(term->line_edit.text + term->line_edit.cur,\
+ft_strlen(term->line_edit.text + term->line_edit.cur));
 	ft_strcat(term->line_edit.text, term->c_buffer);
 	ft_strcat(term->line_edit.text, line);
 	term->line_edit.len = ft_strlen(term->line_edit.text);
