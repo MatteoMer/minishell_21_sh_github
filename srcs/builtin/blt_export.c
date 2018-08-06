eab2512c842f1e2c5b10308f0bd83203d5224738
xmazella <xavier.mazella@gmail.com>
2018-08-06 23:01:07 +0200

    export almost done

--- srcs/builtin/blt_export.c
+++ srcs/builtin/blt_export.c
@@ -6,47 +6,12 @@
 /*   By: xmazella <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/06 16:38:16 by xmazella          #+#    #+#             */
-/*   Updated: 2018/08/06 18:21:36 by xmazella         ###   ########.fr       */
+/*   Updated: 2018/08/06 23:00:24 by xmazella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 
-
-int		blt_export_error(char *cmd, int i)
-{
-	if (i == 0)
-	{
-		ft_putstr("21sh: export: ");
-		ft_putstr(cmd);
-		ft_putendl(": invalid option");
-	}
-	return (1);
-}
-
-int		blt_print_flag(char **cmd)
-{
-	t_env	*tmp;
-
-	tmp = g_env;
-	while (tmp)
-	{	
-		if (((*cmd == NULL || !ft_strncmp(*cmd, tmp->name, ft_strlen(*cmd))) &&\
-					(tmp->type == EXPORT || tmp->type == ENV) ))
-		{
-			ft_putstr("export ");
-			ft_putstr(tmp->name);
-			ft_putstr("=");
-			ft_putendl(tmp->value);
-			if (*cmd && *(cmd + 1) == NULL)
-				break ;
-			if (*cmd)
-				cmd++;
-		}
-		tmp = tmp->next;
-	}
-	return (1);
-}
 
 int		blt_flag_export(char **cmd)
 {
@@ -79,18 +44,25 @@ int		blt_print_export(void)
 
 int			blt_export_env(char **cmd)
 {
-			ft_putstr("coucou           1\n");
-	while (*cmd)
+	t_env	*tmp;
+	char	*str;
+
+	while (*cmd != NULL)
 	{
-			ft_putstr("coucou           2\n");
-		if (ft_strchr(*cmd, '=') == NULL)
+		if (ft_strchr(*cmd, '=') == NULL || (ft_strchr(*cmd, '=') + 1) == NULL)
 		{
-			ft_putstr("coucou           3\n");
-		//	blt_setenv(cmd, &g_env, ENV);
-			blt_setenv(cmd, &g_env, EXPORT);
+			tmp = blt_getenv(*cmd, g_env);
+			if (tmp == NULL)
+			{
+				str = ft_strjoin(*cmd, *cmd[ft_strlen(*cmd) - 1] == '=' ?\
+"''" : "=''");
+				add_varloc(str, EXPORT);
+			}
+			else
+				blt_change_type(*cmd, ENV);
 		}
 		else
-			blt_setenv(cmd, &g_env, ENV);
+			add_varloc(*cmd, ENV);
 		cmd++;
 	}
 	return (1);
--- srcs/var/var.c
+++ srcs/var/var.c
@@ -6,17 +6,18 @@
 /*   By: xmazella <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/02 20:58:56 by xmazella          #+#    #+#             */
-/*   Updated: 2018/08/05 19:02:46 by xmazella         ###   ########.fr       */
+/*   Updated: 2018/08/06 22:58:35 by xmazella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 #include "parse.h"
 
-void			add_varloc(char *str)
+void			add_varloc(char *str, t_type type)
 {
 	char 		**split;
 	char		*string;
+	t_env		*tmp;
 	
 	split = (char **)malloc(sizeof(char *) * 2);
 	split[0] = ft_strdup(str);
@@ -25,10 +26,11 @@ void			add_varloc(char *str)
 	if ((string = ft_strchr(split[0], '=')) == NULL)
 		return ;
 	*string = 0;
-	if (blt_getenv(split[0], g_env) == NULL)
+	tmp = blt_getenv(split[0], g_env);
+	if (tmp == NULL || tmp->type == type)
 	{
 		*string = '=';
-		blt_setenv(split, &g_env, LOCALE);
+		blt_setenv(split, &g_env, type);
 	}
 	else
 	{
@@ -44,7 +46,7 @@ t_list			*set_varloc(t_list *lst)
 	{
 		if ((lst->content_size != 3) || (!ft_strchr(lst->content, '=')))
 			return (lst);
-		add_varloc(lst->content);
+		add_varloc(lst->content, LOCALE);
 		lst = lst->next;
 	}
 	return (lst);
