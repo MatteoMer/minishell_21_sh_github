b9bc7e01ddf9a13e203e1acb98187fb72dc2d5b6
xmazella <xavier.mazella@gmail.com>
2018-08-11 06:48:49 +0200

    all builtin posix ok

--- srcs/builtin/blt_unsetenv.c
+++ srcs/builtin/blt_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: mmervoye <mmervoye@student.42.fd>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 13:21:03 by mmervoye          #+#    #+#             */
-/*   Updated: 2018/08/11 03:06:52 by xmazella         ###   ########.fr       */
+/*   Updated: 2018/08/11 06:48:11 by xmazella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int				blt_unsetenv(char **cmd)
 {
-	blt_setenv(cmd, &(g_env), LOCALE, 0);
+	t_env *tmp;
+
+	tmp = g_env;
+	while (tmp)
+	{
+		if (ft_strcmp(tmp->name, *cmd) == 0)
+		{
+			tmp->type = LOCALE;
+			break;
+		}
+		tmp = tmp->next;
+	}
 	return (1);
 }
